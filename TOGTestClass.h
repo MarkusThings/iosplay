6d97d2d1e4a4e4f48a5ff8ab973b8e27ff9bf507
Markus Breuer <markus.breuer@gmail.com>
2014-03-02 14:19:46 +0100

    new class for testing

---
+++ TOGTestClass.h
@@ -0,0 +1,13 @@
+//
+//  TOGTestClass.h
+//  GitTest
+//
+//  Created by Markus Breuer on 02.03.14.
+//  Copyright (c) 2014 Markus Breuer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TOGTestClass : NSObject
+
+@end
---
+++ TOGTestClass.m
@@ -0,0 +1,13 @@
+//
+//  TOGTestClass.m
+//  GitTest
+//
+//  Created by Markus Breuer on 02.03.14.
+//  Copyright (c) 2014 Markus Breuer. All rights reserved.
+//
+
+#import "TOGTestClass.h"
+
+@implementation TOGTestClass
+
+@end
